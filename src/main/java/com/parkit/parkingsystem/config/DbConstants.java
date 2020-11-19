4fa3732a42670d9160b19a5c8b594e127a751146
David <zair.david@outlook.fr>
2020-11-19 22:17:51 +0100

    Refactorization of the class FareCalculatorService

---
+++ src/main/java/com/parkit/parkingsystem/config/DbConstants.java
@@ -0,0 +1,10 @@
+package com.parkit.parkingsystem.config;
+
+public class DbConstants {
+
+    public static String checkOccurenceQuery(String vehicleRegNumber) {
+        return "SELECT COUNT(*) AS TOTAL FROM ticket" +
+                "WHERE VEHICLE_REG_NUMBER = '" + vehicleRegNumber + "' AND OUT_TIME IS NOT NULL";
+    }
+
+}
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static com.parkit.parkingsystem.config.DbConstants.checkOccurenceQuery;
+
 public class FareCalculatorService {
 
     DataBaseConfig dataBaseTestConfig = new DataBaseConfig();
@@ -18,7 +20,7 @@ public class FareCalculatorService {
             connection = dataBaseTestConfig.getConnection();
 
             //set parking entries to available
-            ResultSet hasCame = connection.createStatement().executeQuery("SELECT COUNT(*) AS TOTAL FROM ticket WHERE VEHICLE_REG_NUMBER = '" + vehicleRegNumber + "'");
+            ResultSet hasCame = connection.createStatement().executeQuery(checkOccurenceQuery(vehicleRegNumber));
             hasCame.next();
             if (hasCame.getInt(1) > 0) {
                 return true;
@@ -42,36 +44,27 @@ public class FareCalculatorService {
 
         double inHour = ticket.getInTime().getTime();
         double outHour = ticket.getOutTime().getTime();
+        double pourcent = 1;
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
         double duration = (outHour - inHour) / (60 * 60 * 1000);
 
+        if (checkIfCameBefore(ticket.getVehicleRegNumber())) {
+            pourcent = 0.95;
+        }
+
         if (duration <= 0.5) {
             ticket.setPrice(0);
-        }
-        else if (checkIfCameBefore(ticket.getVehicleRegNumber())) {
-            switch (ticket.getParkingSpot().getParkingType()) {
-                case CAR: {
-                    ticket.setPrice((duration * Fare.CAR_RATE_PER_HOUR) * 0.95);
-                    break;
-                }
-                case BIKE: {
-                    ticket.setPrice((duration * Fare.BIKE_RATE_PER_HOUR) * 0.95);
-                    break;
-                }
-                default:
-                    throw new IllegalArgumentException("Unkown Parking Type");
-            }
         }
         else {
 
             switch (ticket.getParkingSpot().getParkingType()) {
                 case CAR: {
-                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR * pourcent);
                     break;
                 }
                 case BIKE: {
-                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR * pourcent);
                     break;
                 }
                 default:
