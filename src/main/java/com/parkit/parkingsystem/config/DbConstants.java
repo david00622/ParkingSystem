3851a280a7662c0f8cb3d08bea365b160d1ec102
David <zair.david@outlook.fr>
2020-12-04 13:01:23 +0100

    Updating plugins and dependencies.

--- src/main/java/com/parkit/parkingsystem/config/DbConstants.java
+++ src/main/java/com/parkit/parkingsystem/config/DbConstants.java
@@ -3,7 +3,7 @@ package com.parkit.parkingsystem.config;
 public class DbConstants {
 
     public static String checkOccurenceQuery(String vehicleRegNumber) {
-        return "SELECT COUNT(*) AS TOTAL FROM ticket" +
+        return "SELECT COUNT(*) AS TOTAL FROM ticket " +
                 "WHERE VEHICLE_REG_NUMBER = '" + vehicleRegNumber + "' AND OUT_TIME IS NOT NULL";
     }
 
